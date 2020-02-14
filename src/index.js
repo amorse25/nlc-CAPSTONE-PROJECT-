fec39a8335e3b8f909f1e581d109324b877fe3fa
Adam Morse <Morse1@missouristate.edu>
2020-02-14 13:20:20 -0600

    Add github pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
